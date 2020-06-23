133e835af4a0c2ad21fb58cf2de5104eeffcc11c
CGrassin <42406777+CGrassin@users.noreply.github.com>
2020-06-23 16:30:30 +0200

    Updated project to Androidx

--- ElectroDB/app/src/main/java/fr/charleslabs/electrodb/component/SearchResult.java
+++ ElectroDB/app/src/main/java/fr/charleslabs/electrodb/component/SearchResult.java
@@ -1,7 +1,7 @@
 package fr.charleslabs.electrodb.component;
 
 
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 
 import java.util.ArrayList;
 import java.util.Arrays;
--- ElectroDB/app/src/main/java/fr/charleslabs/electrodb/componentViews/PinoutTable.java
+++ ElectroDB/app/src/main/java/fr/charleslabs/electrodb/componentViews/PinoutTable.java
@@ -2,7 +2,7 @@ package fr.charleslabs.electrodb.componentViews;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.Gravity;
